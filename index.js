49ed02c9a534fe7dc1671b7eecacd4541a788e24
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-27 17:04:15 -0700

    commit tree ef61d877eaf6d4433941c0638d43ece5d51cbee3

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
   
+});
+
+function fetchBooks() {
+  return fetch ('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json()) 
+    .then(jsonObj => renderBooks(jsonObj))
+
 }
 
+
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
